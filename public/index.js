ff5b4696aea28d81cf7817c386da7bce650ccb4c
unknown <john@capovani.com>
2022-09-18 23:41:32 -0400

    Added admin update screen.

--- public/index.js
+++ public/index.js
@@ -26,3 +26,23 @@ function renderBook(book) {
 }
 
 main()
+updateBook()
+
+
+async function updateBook(){
+
+let response = await fetch('http://localhost:3001/updateBook',{
+    
+method: "PATCH",
+
+headers: {'Content-Type': 'application/json'},
+
+body: JSON.stringify({
+
+    "id": 3,
+    "title": "Legends of Arathrae",
+}),
+
+})
+}
+
